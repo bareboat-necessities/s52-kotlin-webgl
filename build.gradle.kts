cd72306cf4d5ccbdb6b9872d6212e421adf8464c
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2026-06-09 19:10:56 -0400

    updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
 }
 
-val defaultProjectVersion = "0.5.0-SNAPSHOT"
+val defaultProjectVersion = "0.5.1-SNAPSHOT"
 val releaseLabelVersion: String? = providers.gradleProperty("releaseLabel")
     .orElse(providers.environmentVariable("S52_RELEASE_LABEL"))
     .orElse(providers.environmentVariable("GITHUB_REF_NAME"))
